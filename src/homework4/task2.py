5ed6b559bd7833ba1addd3f0458d84700cea2cd8
Danila Kritsky <danilakritsky@gmail.com>
2021-11-04 01:15:36 +0300

    Update task2.py: substitute print with assert

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -12,14 +12,18 @@ List practice.
 
 def main() -> None:
     """Perform list transformations according to the task instructions."""
-    print(orig_ls := [a + b for a in 'ab' for b in 'bcd'])
-    print(orig_ls[::2])
-    print(orig_ls := [str(num) + 'a' for num in range(1, 5)])
+    orig_ls = [a + b for a in 'ab' for b in 'bcd']
+    assert orig_ls == ['ab', 'ac', 'ad', 'bb', 'bc', 'bd'], "Expected ['ab', 'ac', 'ad', 'bb', 'bc', 'bd']."
+    assert orig_ls[::2] == ['ab', 'ad', 'bc'], "Expected ['ab', 'ad', 'bc']."
+
+    orig_ls = [str(num) + 'a' for num in range(1, 5)]
+    assert orig_ls == ['1a', '2a', '3a', '4a'], "Expected ['1a', '2a', '3a', '4a']."
+
     print(popped := orig_ls.pop(1))
     copy_ls = orig_ls.copy()
     copy_ls.insert(1, popped)
-    print(copy_ls)
-    print(orig_ls)
+    assert copy_ls == ['1a', '2a', '3a', '4a'], "Expected ['1a', '2a', '3a', '4a']."
+    assert orig_ls == ['1a', '3a', '4a'], "Expected ['1a', '3a', '4a']."
 
 
 if __name__ == '__main__':
