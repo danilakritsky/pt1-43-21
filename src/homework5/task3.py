818d57917d7913eb67764e409bd9135b4c8c70ee
Danila Kritsky <danilakritsky@gmail.com>
2021-11-27 22:34:46 +0300

    Refactor task3.py, test_task3.py

--- src/homework5/task3.py
+++ src/homework5/task3.py
@@ -6,6 +6,10 @@
 
 def intersection_count(first_list: list[int], second_list: list[int]) -> int:
     """Return the count of unique numbers that are present in both supplied lists."""
+    for input_list in (first_list, second_list):
+        if not (isinstance(input_list, list) and all(isinstance(elem, int) for elem in input_list)):
+            raise TypeError('Expected list of ints.')
+
     unique_count = len(set(first_list) & set(second_list))
     print(f'Count of unique items: {unique_count}')
     return unique_count
--- src/homework5/test_task3.py
+++ src/homework5/test_task3.py
@@ -4,9 +4,14 @@ import unittest
 from unittest.case import TestCase
 from unittest.mock import patch
 
+from ddt import data    # type: ignore
+from ddt import ddt     # type: ignore
+from ddt import unpack  # type: ignore
+
 from task3 import intersection_count
 
 
+@ddt
 class TestIntersectionCount(TestCase):
     """Test cases for the intersection_count() function."""
 
@@ -18,24 +23,26 @@ class TestIntersectionCount(TestCase):
             mock_print.assert_called_with('Count of unique items: 1')
         )
 
-    def test_output(self) -> None:
+    @data(
+        ([], 0),
+        ([1, 2, 3, 4, 5], 5),
+        ([8, 9, 10], 3),
+        ([1, 4, 7, 10], 4),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] * 3, 10)
+    )
+    @unpack
+    def test_output(self, second_list, expected_output):
         """Test the intersection_count() function."""
         first_list = list(range(1, 11))
-        empty_list: list = []
-        test_data = [
-            (empty_list, 0),
-            (first_list[:5], 5),
-            (first_list[-3:], 3),
-            (first_list[::3], 4),
-            (first_list * 3, 10)
-        ]
+        self.assertEqual(
+            intersection_count(first_list, second_list),
+            expected_output
+        )
 
-        for second_list, expected_output in test_data:
-            with self.subTest():
-                self.assertEqual(
-                    intersection_count(first_list, second_list),
-                    expected_output
-                )
+    @data(int, (1, 2), [1, 'a', 3])
+    def test_wrong_type(self, second_arg):
+        """Test for wrong input type."""
+        self.assertRaises(TypeError, intersection_count, [1, 2], second_arg)
 
 
 if __name__ == '__main__':
