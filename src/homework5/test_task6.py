1db777bb9b7058c1211ccaff2c4434227bfc1d3b
Danila Kritsky <danilakritsky@gmail.com>
2021-11-28 20:33:30 +0300

    Update test_task6.py: change parameter name to avoid pylint errors

--- src/homework5/test_task6.py
+++ src/homework5/test_task6.py
@@ -12,16 +12,14 @@ class TestMain(TestCase):
     """Tests for the main() function."""
 
     @patch('builtins.input', side_effect=[''])
-    def test_no_input(self, mock_input):
-        # pylint: disable=W0613
+    def test_no_input(self, _):
         """Test the output of main() when no text is input."""
         self.assertRaises(SystemExit, main)
 
     @patch('builtins.input', side_effect=['cat dog cat', 'dog cat bird', ''])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_output(self, mock_stdout, mock_input):
+    def test_output(self, mock_stdout, _):
         """Test the output of the main() function."""
-        # pylint: disable=W0613
         main()
         self.assertEqual(mock_stdout.getvalue(), "Unique words count: 3\n")
 
